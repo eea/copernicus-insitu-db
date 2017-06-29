c2d81a9a20576b53fabd7e867edcd955b1e02762
Andreea Dima <andreeadima95@gmail.com>
2017-06-29 16:47:34 +0300

    [refs #85812] Fix tests

--- insitu/tests/test_product.py
+++ insitu/tests/test_product.py
@@ -6,6 +6,7 @@ from insitu.tests import factories
 
 REQUIRED_ERROR = ['This field is required.']
 
+# CHECK SUCCESS URL
 
 class ProductTests(TestCase):
     def setUp(self):
@@ -13,6 +14,7 @@ class ProductTests(TestCase):
         self.related_fields = ['group', 'component', 'status', 'coverage']
         self.required_fields = ['acronym', 'name', 'group', 'component',
                                 'status', 'coverage']
+
         group = factories.ProductGroupFactory()
         component = factories.ComponentFactory()
         status = factories.ProductStatusFactory()
@@ -34,6 +36,7 @@ class ProductTests(TestCase):
         resp = self.client.post(reverse('product:add'), data)
         self.assertEqual(resp.status_code, 200)
         self.assertIsNot(resp.context['form'].errors, {})
+
         errors = {field: REQUIRED_ERROR for field in self.required_fields}
         self.assertDictEqual(resp.context['form'].errors, errors)
 
@@ -41,6 +44,7 @@ class ProductTests(TestCase):
         data = self._PRODUCT_DATA
         resp = self.client.post(reverse('product:add'), data)
         self.assertEqual(resp.status_code, 302)
+
         qs = models.Product.objects.all()
         self.assertEqual(qs.count(), 1)
         product = qs.first()
@@ -53,6 +57,7 @@ class ProductTests(TestCase):
         factories.ProductFactory()
         resp = self.client.get(reverse('product:json'))
         self.assertEqual(resp.status_code, 200)
+
         data = resp.json()
         self.assertIs(data['recordsTotal'], 1)
         self.assertIs(data['recordsFiltered'], 1)
@@ -62,3 +67,19 @@ class ProductTests(TestCase):
         resp = self.client.get(reverse('product:detail',
                                kwargs={'pk': product.pk}))
         self.assertEqual(resp.context['product'], product)
+
+    def test_edit_product(self):
+        product = factories.ProductFactory()
+        data = self._PRODUCT_DATA
+        resp = self.client.post(
+            reverse('product:edit', kwargs={'pk': product.pk}),
+            data)
+        self.assertEqual(resp.status_code, 302)
+
+        qs = models.Product.objects.all()
+        self.assertEqual(qs.count(), 1)
+        product = qs.first()
+        for field in self.fields:
+            self.assertEqual(getattr(product, field), data[field])
+        for related_field in self.related_fields:
+            self.assertEqual(getattr(product, related_field).pk, data[related_field])
