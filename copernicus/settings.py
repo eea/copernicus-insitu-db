66fea66eadd126f8974b1c2e27da42c80aab1fda
Catalin Jitea <catalinjitea@gmail.com>
2017-07-10 16:10:41 +0300

    Add Django Suit theme for admin interface

--- copernicus/settings.py
+++ copernicus/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', env('ALLOWED_HOSTS')]
 # Application definition
 
 INSTALLED_APPS = [
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
