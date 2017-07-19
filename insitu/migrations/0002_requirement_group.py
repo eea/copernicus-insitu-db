203540c0cc20e9f07caba7b4d358dacba7c3eef3
Diana Boiangiu <boiangiudiana58@gmail.com>
2017-07-19 16:56:58 +0300

    [refs #86831] Add default for requirement group

--- insitu/migrations/0002_requirement_group.py
+++ insitu/migrations/0002_requirement_group.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='requirement',
             name='group',
-            field=models.ForeignKey(default=100, on_delete=django.db.models.deletion.CASCADE, to='picklists.RequirementGroup'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='picklists.RequirementGroup'),
             preserve_default=False,
         ),
     ]
