4cf05e6bd3b1c206311bbb6d374dfbb444086c39
Andreea Dima <andreeadima95@gmail.com>
2017-07-20 15:58:48 +0300

    [refs #86844] Help section

---
+++ insitu/templatetags/utils.py
@@ -0,0 +1,22 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_metric_value(form, field, attribute):
+    if form.data:
+        return form.data["_".join([field, attribute])]
+    elif form.initial:
+        return form.initial["_".join([field, attribute])]
+
+
+@register.filter(name='get_field_nice_value')
+def get_model_attribute(object, attribute):
+    field = object._meta.get_field(attribute)
+    if field.choices:
+        for value, choice in field.flatchoices:
+            if value == getattr(object, attribute):
+                return choice
+    return getattr(object, attribute)
--- insitu/views/management.py
+++ insitu/views/management.py
@@ -2,9 +2,38 @@ from django.urls import reverse_lazy
 
 from insitu.views import protected
 from insitu.views.protected.views import ProtectedTemplateView
+from picklists import models
 
 
 class PicklistsManager(ProtectedTemplateView):
     template_name = 'picklists.html'
-    permission_classes = (protected.IsSuperuser, )
+    permission_classes = (protected.IsSuperuser,)
     permission_denied_redirect = reverse_lazy('auth:login')
+
+
+class HelpPage(ProtectedTemplateView):
+    template_name = 'help.html'
+    permission_classes = (protected.IsAuthenticated,)
+    permission_denied_redirect = reverse_lazy('auth:login')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['models'] = dict()
+
+        PICKLISTS = [
+            models.Barrier, models.ComplianceLevel, models.Coverage, models.Criticality,
+            models.Country, models.DataFormat, models.DataType, models.DefinitionLevel,
+            models.Dissemination, models.EssentialVariable, models.UpdateFrequency,
+            models.InspireTheme, models.ProductGroup, models.RequirementGroup,
+            models.ProductStatus, models.Relevance, models.Quality, models.Timeliness,
+            models.Policy
+        ]
+
+        for model in PICKLISTS:
+            data = {
+                'nice_name': model._meta.verbose_name,
+                'manager': model.objects,
+                'fields': [field.name for field in model._meta.fields]
+            }
+            context['models'][model._meta.model_name] = data
+        return context
