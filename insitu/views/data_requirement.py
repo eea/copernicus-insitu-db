31174dabe202356a2a211c50494d2a936ef5897e
Andreea Dima <andreeadima95@gmail.com>
2017-10-23 15:38:57 +0300

    [refs #89215] Cleanup unused code

--- insitu/views/data_requirement.py
+++ insitu/views/data_requirement.py
@@ -1,4 +1,4 @@
-from django.core.urlresolvers import reverse
+from django.urls import reverse_lazy
 
 from insitu import models
 from insitu import forms
@@ -9,56 +9,29 @@ from insitu.views.protected.permissions import IsCopernicusServiceResponsible
 
 class DataRequirementAdd(ProtectedCreateView):
     template_name = 'data/requirement/add.html'
-    permission_classes = (IsCopernicusServiceResponsible, )
-
-    def permission_denied(self, request):
-        if 'data_pk' in self.kwargs:
-            self.permission_denied_redirect = reverse('data:list')
-        else:
-            self.permission_denied_redirect = reverse('requirement:list')
-        return super().permission_denied(request)
-
-    def _set_model_used(self):
-        if 'data_pk' in self.kwargs:
-            self.form_class = forms.DataRequirementForm
-            self.form_field = 'data'
-            self.model = models.Data
-            self.title = "Add a new requirement for {}"
-            self.pk = self.kwargs['data_pk']
-        else:
-            self.form_class = forms.RequirementDataRequirementForm
-            self.form_field = 'requirement'
-            self.model = models.Requirement
-            self.title = "Add a new data for {}"
-            self.pk = self.kwargs['requirement_pk']
+    permission_classes = (IsCopernicusServiceResponsible,)
+    permission_denied_redirect = reverse_lazy('requirement:list')
+    form_class = forms.RequirementDataRequirementForm
+    form_field = 'requirement'
+    model = models.Requirement
+    title = "Add a new data for {}"
 
     def get_form(self):
         form = super().get_form(self.form_class)
-        form.fields[self.form_field].initial = self.pk
+        form.fields[self.form_field].initial = self.kwargs['requirement_pk']
         return form
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context[self.form_field] = self.model.objects.get(pk=self.pk)
+        context[self.form_field] = self.model.objects.get(
+            pk=self.kwargs['requirement_pk'])
         context['title'] = self.title.format(context[self.form_field].name)
         context['url'] = self.get_success_url()
         return context
 
     def get_success_url(self):
-        if 'data_pk' in self.kwargs:
-            return reverse('data:detail',
-                           kwargs={'pk': self.kwargs['data_pk']})
-        else:
-            return reverse('requirement:detail',
-                           kwargs={'pk': self.kwargs['requirement_pk']})
-
-    def get(self, request, *args, **kwargs):
-        self._set_model_used()
-        return super().get(self, request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        self._set_model_used()
-        return super().post(self, request, *args, **kwargs)
+        return reverse_lazy('requirement:detail',
+                            kwargs={'pk': self.kwargs['requirement_pk']})
 
 
 class DataRequirementEdit(ProtectedUpdateView):
@@ -66,31 +39,17 @@ class DataRequirementEdit(ProtectedUpdateView):
     template_name = 'data/requirement/edit.html'
     form_class = forms.DataRequirementEditForm
     context_object_name = 'rel'
-    permission_classes = (IsCopernicusServiceResponsible, )
-
-    def permission_denied(self, request):
-        if 'data_pk' in self.kwargs:
-            self.permission_denied_redirect = reverse('data:list')
-        else:
-            self.permission_denied_redirect = reverse('requirement:list')
-        return super().permission_denied(request)
-
-    def _get_reverse_url(self):
-        if 'data_pk' in self.kwargs:
-            url = reverse('data:detail',
-                          kwargs={'pk': self.kwargs['data_pk']})
-        else:
-            url = reverse('requirement:detail',
-                          kwargs={'pk': self.kwargs['requirement_pk']})
-        return url
+    permission_classes = (IsCopernicusServiceResponsible,)
+    permission_denied_redirect = reverse_lazy('requirement:list')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['url'] = self._get_reverse_url()
+        context['url'] = self.get_success_url()
         return context
 
     def get_success_url(self):
-        return self._get_reverse_url()
+        return reverse_lazy('requirement:detail',
+                            kwargs={'pk': self.kwargs['requirement_pk']})
 
 
 class DataRequirementDelete(ProtectedDeleteView):
@@ -98,28 +57,14 @@ class DataRequirementDelete(ProtectedDeleteView):
     template_name = 'data/requirement/delete.html'
     form_class = forms.DataRequirementEditForm
     context_object_name = 'rel'
-    permission_classes = (IsCopernicusServiceResponsible, )
-
-    def permission_denied(self, request):
-        if 'data_pk' in self.kwargs:
-            self.permission_denied_redirect = reverse('data:list')
-        else:
-            self.permission_denied_redirect = reverse('requirement:list')
-        return super().permission_denied(request)
-
-    def _get_reverse_url(self):
-        if 'data_pk' in self.kwargs:
-            url = reverse('data:detail',
-                          kwargs={'pk': self.kwargs['data_pk']})
-        else:
-            url = reverse('requirement:detail',
-                          kwargs={'pk': self.kwargs['requirement_pk']})
-        return url
+    permission_classes = (IsCopernicusServiceResponsible,)
+    permission_denied_redirect = reverse_lazy('requirement:list')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['url'] = self._get_reverse_url()
+        context['url'] = self.get_success_url()
         return context
 
     def get_success_url(self):
-        return self._get_reverse_url()
+        return reverse_lazy('requirement:detail',
+                            kwargs={'pk': self.kwargs['requirement_pk']})
--- insitu/views/data_responsible_relation.py
+++ insitu/views/data_responsible_relation.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse_lazy
+
 from insitu.views.protected import IsCopernicusServiceResponsible
 from insitu.views.protected import (
     ProtectedUpdateView, ProtectedCreateView, ProtectedDeleteView)
@@ -12,57 +13,28 @@ from insitu import forms
 
 class DataDataResponsibleAdd(ProtectedCreateView):
     template_name = 'data/data_responsible/add.html'
-    permission_classes = (IsCopernicusServiceResponsible, )
-
-    def permission_denied(self, request):
-        if 'responsible_pk' in self.kwargs:
-            self.permission_denied_redirect = reverse('responsible:list')
-        else:
-            self.permission_denied_redirect = reverse('data:list')
-        return super().permission_denied(request)
-
-    def _set_model_used(self):
-        if 'responsible_pk' in self.kwargs:
-            self.form_class = forms.DataResponsibleRelationResponsibleForm
-            self.form_field = 'responsible'
-            self.model = models.DataResponsible
-            self.title = "Add a new group for {}"
-            self.pk = self.kwargs['responsible_pk']
-        else:
-            self.form_class = forms.DataResponsibleRelationGroupForm
-            self.form_field = 'data'
-            self.model = models.Data
-            self.title = "Add a new responsible for {}"
-            self.pk = self.kwargs['group_pk']
+    permission_classes = (IsCopernicusServiceResponsible,)
+    permission_denied_redirect = reverse_lazy('data:list')
+    form_class = forms.DataResponsibleRelationGroupForm
+    form_field = 'data'
+    model = models.Data
+    title = "Add a new responsible for {}"
 
     def get_form(self):
         form = super().get_form(self.form_class)
-        form.fields[self.form_field].initial = self.pk
+        form.fields[self.form_field].initial = self.kwargs['group_pk']
         return form
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context[self.form_field] = self.model.objects.get(pk=self.pk)
+        context[self.form_field] = self.model.objects.get(pk=self.kwargs['group_pk'])
         context['title'] = self.title.format(context[self.form_field].name)
         context['url'] = self.get_success_url()
         return context
 
     def get_success_url(self):
-        if 'responsible_pk' in self.kwargs:
-            return reverse('responsible:detail',
-                           kwargs={'pk': self.kwargs['responsible_pk']})
-        else:
-
-            return reverse('data:detail',
-                           kwargs={'pk': self.kwargs['group_pk']})
-
-    def get(self, request, *args, **kwargs):
-        self._set_model_used()
-        return super().get(self, request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        self._set_model_used()
-        return super().post(self, request, *args, **kwargs)
+        return reverse_lazy('data:detail',
+                            kwargs={'pk': self.kwargs['group_pk']})
 
 
 class DataDataResponsibleEdit(ProtectedUpdateView):
@@ -70,59 +42,31 @@ class DataDataResponsibleEdit(ProtectedUpdateView):
     template_name = 'data/data_responsible/edit.html'
     form_class = forms.DataResponsibleRelationEditForm
     context_object_name = 'rel'
-    permission_classes = (IsCopernicusServiceResponsible, )
-
-    def permission_denied(self, request):
-        if 'responsible_pk' in self.kwargs:
-            self.permission_denied_redirect = reverse('responsible:list')
-        else:
-            self.permission_denied_redirect = reverse('data:list')
-        return super().permission_denied(request)
-
-    def _get_reverse_url(self):
-        if 'responsible_pk' in self.kwargs:
-            url = reverse('responsible:detail',
-                       kwargs={'pk': self.kwargs['responsible_pk']})
-        else:
-            url = reverse('data:detail',
-                       kwargs={'pk': self.kwargs['group_pk']})
-        return url
+    permission_classes = (IsCopernicusServiceResponsible,)
+    permission_denied_redirect = reverse_lazy('data:list')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['url'] = self._get_reverse_url()
+        context['url'] = self.get_success_url()
         return context
 
     def get_success_url(self):
-        return self._get_reverse_url()
+        return reverse_lazy('data:detail',
+                            kwargs={'pk': self.kwargs['group_pk']})
 
 
 class DataDataResponsibleDelete(ProtectedDeleteView):
     model = models.DataResponsibleRelation
     template_name = 'data/data_responsible/delete.html'
     context_object_name = 'rel'
-    permission_classes = (IsCopernicusServiceResponsible, )
-
-    def permission_denied(self, request):
-        if 'responsible_pk' in self.kwargs:
-            self.permission_denied_redirect = reverse('responsible:list')
-        else:
-            self.permission_denied_redirect = reverse('data:list')
-        return super().permission_denied(request)
-
-    def _get_reverse_url(self):
-        if 'responsible_pk' in self.kwargs:
-            url = reverse('responsible:detail',
-                          kwargs={'pk': self.kwargs['responsible_pk']})
-        else:
-            url = reverse('data:detail',
-                          kwargs={'pk': self.kwargs['group_pk']})
-        return url
+    permission_classes = (IsCopernicusServiceResponsible,)
+    permission_denied_redirect = reverse_lazy('data:list')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['url'] = self._get_reverse_url()
+        context['url'] = self.get_success_url()
         return context
 
     def get_success_url(self):
-        return self._get_reverse_url()
+        return reverse_lazy('data:detail',
+                            kwargs={'pk': self.kwargs['group_pk']})
