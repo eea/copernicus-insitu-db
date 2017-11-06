0c97088e8492af98ce5ecca8fd862cc8fd53c2ff
Diana Boiangiu <boiangiudiana58@gmail.com>
2017-11-06 17:43:22 +0200

    [refs #89206] Fix google analytics

--- insitu/context_processors.py
+++ insitu/context_processors.py
@@ -4,11 +4,9 @@ from getenv import env
 
 def google_analytics(request):
     ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', False)
-    ga_domain = getattr(settings, 'GOOGLE_ANALYTICS_DOMAIN', False)
     if not settings.DEBUG and ga_prop_id:
         return {
             'GOOGLE_ANALYTICS_PROPERTY_ID': ga_prop_id,
-            'GOOGLE_ANALYTICS_DOMAIN': ga_domain,
         }
     return {}
 
