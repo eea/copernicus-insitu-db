6d5833e2ceace8071054bda1392be8e07bb61fe2
Diana Boiangiu <boiangiudiana58@gmail.com>
2017-11-21 16:05:43 +0200

    [refs #90127] Display deleted rows in admin relation models

--- insitu/admin.py
+++ insitu/admin.py
@@ -18,15 +18,35 @@ class ComponentAdmin(admin.ModelAdmin):
     list_display = ('name', 'service', 'entrusted_entity')
 
 
+class BaseDisplayDeleteAdminMixin:
+    list_display = ('__str__', '_deleted',)
+    list_filter = ('_deleted',)
+    filter_model = None
+
+    def get_queryset(self, request):
+        return self.filter_model.objects.really_all()
+
+
+class ProductRequirementAdmin(BaseDisplayDeleteAdminMixin, admin.ModelAdmin):
+    filter_model = models.ProductRequirement
+
+
+class DataRequirementAdmin(BaseDisplayDeleteAdminMixin, admin.ModelAdmin):
+    filter_model = models.DataRequirement
+
+
+class DataProviderRelationAdmin(BaseDisplayDeleteAdminMixin, admin.ModelAdmin):
+    filter_model = models.DataProviderRelation
+
 admin.site.register(models.CopernicusService, CopernicusServiceAdmin)
 admin.site.register(models.EntrustedEntity, EntrustedEntityAdmin)
 admin.site.register(models.Component, ComponentAdmin)
 admin.site.register(models.Requirement)
 admin.site.register(models.Product)
-admin.site.register(models.ProductRequirement)
+admin.site.register(models.ProductRequirement, ProductRequirementAdmin)
 admin.site.register(models.DataProvider)
 admin.site.register(models.DataProviderDetails)
 admin.site.register(models.Data)
-admin.site.register(models.DataRequirement)
-admin.site.register(models.DataProviderRelation)
+admin.site.register(models.DataRequirement, DataRequirementAdmin)
+admin.site.register(models.DataProviderRelation, DataProviderRelationAdmin)
 admin.site.register(models.Metric)
