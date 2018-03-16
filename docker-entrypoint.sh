c9ee52bb8fa7ff6f9142d4ed0bf1032a6f9e36c2
Diana Boiangiu <boiangiudiana58@gmail.com>
2018-03-16 16:09:32 +0200

    Fix worker timeout

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,6 +11,10 @@ while ! nc -z $POSTGRES_ADDR 5432; do
   sleep 3s
 done
 
+if [ -z "$TIMEOUT" ]; then
+    TIMEOUT=30
+fi
+
 if [ -z "$1" ]; then
   python manage.py migrate &&
   python manage.py collectstatic --noinput &&
@@ -18,6 +22,7 @@ if [ -z "$1" ]; then
          --name insitu \
          --bind 0.0.0.0:8000 \
          --workers 3 \
+         --timeout $TIMEOUT \
          --access-logfile - \
          --error-logfile -
 fi
