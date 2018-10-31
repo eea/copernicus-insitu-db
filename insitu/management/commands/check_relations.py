f32d8d93ffa9abe35337fb32c72dd10812c01782
Diana Boiangiu <boiangiudiana58@gmail.com>
2018-10-31 14:36:22 +0200

    [refs #99624] Add command to fix dataprovider relations

---
+++ insitu/management/commands/check_relations.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from insitu.models import  Data, DataProvider, Requirement
+
+
+class Command(BaseCommand):
+    help = 'Use in case the data provider relation state gets out of sync from the data state.'
+
+    def handle(self, *args, **options):
+        data_list = Data.objects.all()
+        faulty_data_providers = []
+        for data in data_list:
+            faulty_data_providers.extend(
+                [rel for rel in data.dataproviderrelation_set.all() if rel.state != data.state]
+            )
+        
+        for rel in faulty_data_providers:
+            print("Faulty relation: Data:{} - Data Provider:{} , state:{} , data state: {}".format(
+               rel.data.id,
+               rel.provider.id,
+               rel.state,
+               rel.data.state
+            ))
+            rel.state = rel.data.state
+            rel.save()
+            print("Relation reseted")
+            
