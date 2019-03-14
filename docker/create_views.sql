149f6c857e4764fdf7cd9206e02b7396741bf3ff
Diana Boiangiu <boiangiudiana58@gmail.com>
2019-03-14 14:45:07 +0200

    [refs #103803] Add note to views and report

--- docker/create_views.sql
+++ docker/create_views.sql
@@ -76,6 +76,7 @@ CREATE VIEW insitu_datarequirement_view as
 CREATE VIEW insitu_data_view as
     SELECT d.id AS "data_id",
            d.name AS "data_name",
+           d.note AS "data_note",
            a.name AS "data_area",
            df.name AS "data_format",
            dt.name AS "data_type",
