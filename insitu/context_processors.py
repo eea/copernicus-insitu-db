e0f8e64ae493c897e1981f3a30a130947145b4c0
thorion21 <blazegamingro@gmail.com>
2019-08-27 12:02:35 +0300

    Eliminated spaces refs #106

--- insitu/context_processors.py
+++ insitu/context_processors.py
@@ -5,13 +5,11 @@ from django.contrib.auth.models import User
 from django.contrib.sessions.models import Session
 from insitu.models import Product, Requirement, Data, DataProvider
 
-
 def base(request):
     return {
         'DEBUG': settings.DEBUG,
         'READ_ONLY_GROUP': settings.READ_ONLY_GROUP
     }
-
 
 def matomo(request):
     matomo = getattr(settings, 'MATOMO', False)
@@ -21,7 +19,6 @@ def matomo(request):
         }
     return {}
 
-
 def crazy_egg(request):
     crazy_egg_path = getattr(settings, 'CRAZY_EGG', '')
     if not settings.DEBUG and crazy_egg_path:
@@ -29,7 +26,6 @@ def crazy_egg(request):
             'CRAZY_EGG': crazy_egg_path
         }
     return {}
-
 
 def sentry(request):
     sentry_id = ''
@@ -39,7 +35,6 @@ def sentry(request):
         'sentry_id': sentry_id,
         'sentry_public_id': env('SENTRY_PUBLIC_DSN', ''),
     }
-
 
 def statistics(request):
     return {
