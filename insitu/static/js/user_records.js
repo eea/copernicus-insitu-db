1e71f58f54dbd178fc8a3f6bc6889e1026f6f9ce
ClaudiaP <claudia.prd23@gmail.com>
2019-08-29 17:58:43 +0300

    Add Data, DataProviders and Requirements to user record page refs #104

--- insitu/static/js/user_records.js
+++ insitu/static/js/user_records.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
-  $('#id_user_data').DataTable({
+  $('#user_data').DataTable({
     "lengthChange": false,
     "searching": false,
     "bInfo": false,
+    "pageLength": 5,
+  });
+  $('#user_requirements').DataTable({
+    "lengthChange": false,
+    "searching": false,
+    "bInfo": false,
+    "pageLength": 5,
+  });
+  $('#user_providers').DataTable({
+    "lengthChange": false,
+    "searching": false,
+    "bInfo": false,
+    "pageLength": 5,
   });
 });
--- insitu/views/user_records.py
+++ insitu/views/user_records.py
@@ -1,11 +1,35 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
 from insitu.models import Data, DataProvider,DataProviderRelation, DataRequirement, Product, ProductRequirement, Requirement
+from insitu.views import ProtectedTemplateView, IsAuthenticated
 
 
-class UserRecordsView(TemplateView):
+class UserRecordsView(ProtectedTemplateView):
     template_name = 'user_records.html'
+    permission_classes = (IsAuthenticated,)
+    permission_denied_redirect = reverse_lazy('auth:login')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['data_list'] = Data.objects.filter(created_by=self.request.user)
+        current_user = self.request.user
+        context['data_list'] = Data.objects.filter(
+            created_by=current_user
+        )
+        context['providers_list'] = DataProvider.objects.filter(
+            created_by=current_user
+        )
+        context['provider_relations'] = DataProviderRelation.objects.filter(
+            created_by=current_user
+        )
+        context['data_requirements'] = DataRequirement.objects.filter(
+            created_by=current_user
+        )
+        context['product_requirements'] = ProductRequirement.objects.filter(
+            created_by=current_user
+        )
+        context['product_requirements'] = Requirement.objects.filter(
+            created_by=current_user
+        )
+        context['requirements_list'] = Requirement.objects.filter(
+            created_by=current_user
+        )
         return context
