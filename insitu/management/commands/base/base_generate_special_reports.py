49353d54a15e6350fdd653cbe40acad5219cb304
dianaboiangiu <boiangiudiana58@gmail.com>
2020-05-08 12:51:05 +0300

    Optimize special reports generator

--- insitu/management/commands/base/base_generate_special_reports.py
+++ insitu/management/commands/base/base_generate_special_reports.py
@@ -3,37 +3,36 @@ import json
 import os
 import uuid
 import xlsxwriter
+from django.db import connection
 
 from explorer.exporters import ExcelExporter
 
+def result_iter(cursor, arraysize=2000):
+    'An iterator that uses fetchmany to keep memory usage down'
+    while True:
+        results = cursor.fetchmany(arraysize)
+        if not results:
+            break
+        yield results
 
 class ExcelExporterWriteFile(ExcelExporter):
 
     def _get_output(self, query, path,  **kwargs):
-        res = query.execute_query_only()
-
         directory = os.path.dirname(path)
         if not os.path.exists(directory):
             os.makedirs(directory)
-        
         wb = xlsxwriter.Workbook(path, {'constant_memory': True})
         ws = wb.add_worksheet(name=self._format_title())
-        row = 0
-        col = 0
-        header_style = wb.add_format({'bold': True})
-        for header in res.header_strings:
-            ws.write(row, col, header, header_style)
-            col += 1
-        row = 1
-        col = 0
-        for data_row in res.data:
-            for data in data_row:
-                if isinstance(data, datetime) or isinstance(data, uuid.UUID):
-                    data = str(data)
-                if isinstance(data, dict) or isinstance(data, list):
-                    data = json.dumps(data)
-                ws.write(row, col, data)
-                col += 1
-            row += 1
-            col = 0
-        wb.close()
+
+
+        with connection.cursor() as cursor:
+            cursor.execute(query.sql)
+            header_style = wb.add_format({'bold': True})
+            columns = [col[0] for col in cursor.description]
+            ws.write_row(0, 0,  columns)
+            row = 1
+            for result in result_iter(cursor):
+                for data_row in result:
+                    ws.write_row(row, 0, data_row)
+                    row += 1
+            wb.close()
