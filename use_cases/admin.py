b0f108d8c869e6c0cfe33652b3c4c7e2254e8dbc
Gabi <strezea_gabriela@yahoo.com>
2023-08-30 12:37:46 +0300

    Add models for use_cases

--- use_cases/admin.py
+++ use_cases/admin.py
@@ -1,3 +1,27 @@
-# from django.contrib import admin
+from django.contrib import admin
+from use_cases import models
 
-# Register your models here.
+
+@admin.register(models.CopernicusService)
+class CopernicusServiceAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+@admin.register(models.Country)
+class CountryAdmin(admin.ModelAdmin):
+    search_fields = ["name", "code"]
+
+
+@admin.register(models.UseCase)
+class UseCaseAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+
+
+@admin.register(models.Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+@admin.register(models.Reference)
+class ReferenceAdmin(admin.ModelAdmin):
+    search_fields = ["source"]
