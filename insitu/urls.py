4469460390df5f3c0a7e1f2a9fc642c0f86b1cbf
Diana Boiangiu <dianaboiangiu@Dianas-MacBook-Air-2.local>
2023-08-31 15:01:24 +0300

    Upgraded to Django 3.2

--- insitu/urls.py
+++ insitu/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import RedirectView
 from django.views.static import serve
@@ -8,255 +9,210 @@ from insitu import views
 
 
 product_requirement_patterns = [
-    url(r"^add/$", views.ProductRequirementAdd.as_view(), name="add"),
-    url(r"^add-group/$", views.ProductGroupRequirementAdd.as_view(), name="add_group"),
-    url(r"^(?P<pk>[0-9]+)/$", views.ProductRequirementEdit.as_view(), name="edit"),
-    url(
-        r"^(?P<pk>[0-9]+)/delete$",
-        views.ProductRequirementDelete.as_view(),
-        name="delete",
-    ),
+    path("add/", views.ProductRequirementAdd.as_view(), name="add"),
+    path("add-group/", views.ProductGroupRequirementAdd.as_view(), name="add_group"),
+    path("<int:pk>/", views.ProductRequirementEdit.as_view(), name="edit"),
+    path("<int:pk>/delete", views.ProductRequirementDelete.as_view(), name="delete"),
 ]
 
 product_patterns = [
-    url(r"^list/$", views.ProductList.as_view(), name="list"),
-    url(r"^data/$", views.ProductListJson.as_view(), name="json"),
-    url(r"^add/$", views.ProductAdd.as_view(), name="add"),
-    url(r"^(?P<pk>[0-9]+)/$", views.ProductDetail.as_view(), name="detail"),
-    url(r"^(?P<pk>[0-9]+)/edit/$", views.ProductEdit.as_view(), name="edit"),
-    url(r"^(?P<pk>[0-9]+)/delete/$", views.ProductDelete.as_view(), name="delete"),
-    url(r"^export$", views.ExportProductView.as_view(), name="export"),
-    url(r"import$", views.ImportProductsView.as_view(), name="import"),
+    path("list/", views.ProductList.as_view(), name="list"),
+    path("data/", views.ProductListJson.as_view(), name="json"),
+    path("add/", views.ProductAdd.as_view(), name="add"),
+    path("<int:pk>/", views.ProductDetail.as_view(), name="detail"),
+    path("<int:pk>/edit/", views.ProductEdit.as_view(), name="edit"),
+    path("<int:pk>/delete/", views.ProductDelete.as_view(), name="delete"),
+    path("export", views.ExportProductView.as_view(), name="export"),
+    path("import", views.ImportProductsView.as_view(), name="import"),
 ]
 
 data_requirement_patterns = [
-    url(r"^add/$", views.DataRequirementAdd.as_view(), name="add"),
-    url(r"^(?P<pk>[0-9]+)/$", views.DataRequirementEdit.as_view(), name="edit"),
-    url(
-        r"^(?P<pk>[0-9]+)/delete$",
-        views.DataRequirementDelete.as_view(),
-        name="delete",
-    ),
+    path("add/", views.DataRequirementAdd.as_view(), name="add"),
+    path("<int:pk>/", views.DataRequirementEdit.as_view(), name="edit"),
+    path("<int:pk>/delete", views.DataRequirementDelete.as_view(), name="delete"),
 ]
 
 requirement_patterns = [
-    url(r"^list/$", views.RequirementList.as_view(), name="list"),
-    url(r"^data/$", views.RequirementListJson.as_view(), name="json"),
-    url(r"^(?P<pk>[0-9]+)/$", views.RequirementDetail.as_view(), name="detail"),
-    url(r"^(?P<pk>[0-9]+)/edit$", views.RequirementEdit.as_view(), name="edit"),
-    url(r"^(?P<pk>[0-9]+)/delete/$", views.RequirementDelete.as_view(), name="delete"),
-    url(
-        r"^(?P<pk>[0-9]+)/transition/(?P<source>[a-z]+)-to-(?P<target>[a-z]+)/(?P<transition>[a-z\-\_]+)/$",
+    path("list/", views.RequirementList.as_view(), name="list"),
+    path("data/", views.RequirementListJson.as_view(), name="json"),
+    path("<int:pk>/", views.RequirementDetail.as_view(), name="detail"),
+    path("<int:pk>/edit/", views.RequirementEdit.as_view(), name="edit"),
+    path("<int:pk>/delete/", views.RequirementDelete.as_view(), name="delete"),
+    path(
+        "<int:pk>/transition/<str:source>-to-<str:target>/<slug:transition>/",
         views.RequirementTransition.as_view(),
         name="transition",
     ),
-    url(
-        r"^(?P<requirement_pk>[0-9]+)/product/",
+    path(
+        "<int:requirement_pk>/product/",
         include((product_requirement_patterns, "insitu"), namespace="product"),
     ),
-    url(r"^add/$", views.RequirementAdd.as_view(), name="add"),
-    url(
-        r"^(?P<requirement_pk>[0-9]+)/data/",
+    path("add/", views.RequirementAdd.as_view(), name="add"),
+    path(
+        "<int:requirement_pk>/data/",
         include((data_requirement_patterns, "insitu"), namespace="data"),
     ),
-    url(
-        r"^(?P<pk>[0-9]+)/clear_feedback/$",
+    path(
+        "<int:pk>/clear_feedback/",
         views.RequirementClearFeedback.as_view(),
         name="clear_feedback",
     ),
 ]
 
 data_data_provider_patterns = [
-    url(r"^add/$", views.DataDataProviderAdd.as_view(), name="add"),
-    url(r"^(?P<pk>[0-9]+)/$", views.DataDataProviderEdit.as_view(), name="edit"),
-    url(
-        r"^(?P<pk>[0-9]+)/delete$",
-        views.DataDataProviderDelete.as_view(),
-        name="delete",
-    ),
+    path("add/", views.DataDataProviderAdd.as_view(), name="add"),
+    path("<int:pk>/", views.DataDataProviderEdit.as_view(), name="edit"),
+    path("<int:pk>/delete", views.DataDataProviderDelete.as_view(), name="delete"),
 ]
 
 
 data_patterns = [
-    url(r"^list/$", views.DataList.as_view(), name="list"),
-    url(r"^data/$", views.DataListJson.as_view(), name="json"),
-    url(r"^add/$", views.DataAdd.as_view(), name="add"),
-    url(r"^(?P<pk>[0-9]+)/$", views.DataDetail.as_view(), name="detail"),
-    url(r"^(?P<pk>[0-9]+)/edit$", views.DataEdit.as_view(), name="edit"),
-    url(r"^(?P<pk>[0-9]+)/delete/$", views.DataDelete.as_view(), name="delete"),
-    url(
-        r"^(?P<group_pk>[0-9]+)/provider/",
+    path("list/", views.DataList.as_view(), name="list"),
+    path("data/", views.DataListJson.as_view(), name="json"),
+    path("add/", views.DataAdd.as_view(), name="add"),
+    path("<int:pk>/", views.DataDetail.as_view(), name="detail"),
+    path("<int:pk>/edit", views.DataEdit.as_view(), name="edit"),
+    path("<int:pk>/delete/", views.DataDelete.as_view(), name="delete"),
+    path("<int:group_pk>/provider/",
         include((data_data_provider_patterns, "insitu"), namespace="provider"),
     ),
-    url(
-        r"^(?P<pk>[0-9]+)/transition/(?P<source>[a-z]+)-to-(?P<target>[a-z]+)/(?P<transition>[a-z\-\_]+)/$",
+    path("<int:pk>/transition/<str:source>-to-<str:target>/<slug:transition>/",
         views.DataTransition.as_view(),
         name="transition",
     ),
-    url(
-        r"^(?P<pk>[0-9]+)/clear_feedback/$",
-        views.DataClearFeedback.as_view(),
+    path("<int:pk>/clear_feedback/", views.DataClearFeedback.as_view(),
         name="clear_feedback",
     ),
 ]
 
 provider_patterns = [
-    url(r"^list/$", views.DataProviderList.as_view(), name="list"),
-    url(r"^data/$", views.DataProviderListJson.as_view(), name="json"),
-    url(r"^(?P<pk>[0-9]+)/$", views.DataProviderDetail.as_view(), name="detail"),
-    url(r"^add-network/$", views.DataProviderAddNetwork.as_view(), name="add_network"),
-    url(
-        r"^(?P<pk>[0-9]+)/edit-network/$",
+    path("list/", views.DataProviderList.as_view(), name="list"),
+    path("data/", views.DataProviderListJson.as_view(), name="json"),
+    path("<int:pk>/", views.DataProviderDetail.as_view(), name="detail"),
+    path("add-network/", views.DataProviderAddNetwork.as_view(), name="add_network"),
+    path("<int:pk>/edit-network/",
         views.DataProviderEditNetwork.as_view(),
         name="edit_network",
     ),
-    url(
-        r"^(?P<pk>[0-9]+)/edit-network-members/$",
+    path("<int:pk>/edit-network-members/",
         views.DataProviderEditNetworkMembers.as_view(),
         name="edit_network_members",
     ),
-    url(
-        r"^(?P<pk>[0-9]+)/delete-network/$",
+    path("<int:pk>/delete-network/",
         views.DataProviderDeleteNetwork.as_view(),
         name="delete_network",
     ),
-    url(r"^add/$", views.DataProviderAddNonNetwork.as_view(), name="add_non_network"),
-    url(
-        r"^(?P<pk>[0-9]+)/edit/$",
+    path("add/", views.DataProviderAddNonNetwork.as_view(), name="add_non_network"),
+    path("<int:pk>//edit/",
         views.DataProviderEditNonNetwork.as_view(),
         name="edit_non_network",
     ),
-    url(
-        r"^(?P<pk>[0-9]+)/delete/$",
+    path("<int:pk>/delete/",
         views.DataProviderDeleteNonNetwork.as_view(),
         name="delete_non_network",
     ),
-    url(
-        r"^(?P<pk>[0-9]+)/transition/(?P<source>[a-z]+)-to-(?P<target>[a-z]+)/(?P<transition>[a-z\-\_]+)/$",
+    path("<int:pk>/transition/<str:source>-to-<str:target>/<slug:transition>/",
         views.DataProviderTransition.as_view(),
         name="transition",
     ),
-    url(
-        r"^(?P<pk>[0-9]+)/clear_feedback/$",
+    path("<int:pk>/clear_feedback/",
         views.DataProviderClearFeedback.as_view(),
         name="clear_feedback",
     ),
 ]
 
 auth_patterns = [
-    url(r"^login/", views.LoginView.as_view(), name="login"),
-    url(r"^logout/", views.LogoutView.as_view(), name="logout"),
-    url(
-        r"^change-password/",
-        views.ChangePasswordView.as_view(),
-        name="change_password",
-    ),
-    url(r"^edit_teammates/", views.EditTeamMatesView.as_view(), name="edit_teammates"),
-    url(
-        r"^transfer_ownership/",
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("change-password/", views.ChangePasswordView.as_view(), name="change_password"),
+    path("edit_teammates/", views.EditTeamMatesView.as_view(), name="edit_teammates"),
+    path("transfer_ownership/",
         views.TransferOwnership.as_view(),
         name="transfer_ownership",
     ),
-    url(
-        r"^accept_teammate_request/(?P<sender_user>[0-9]+)",
+    path("accept_teammate_request/<int:sender_user>",
         views.AcceptTeammateRequestView.as_view(),
         name="accept_request",
     ),
-    url(
-        r"^delete_teammate/(?P<teammate_id>[0-9]+)",
+    path("delete_teammate/<int:teammate_id>",
         views.DeleteTeammateView.as_view(),
         name="delete_teammate",
     ),
 ]
 
 reports_patterns = [
-    url(r"^list/$", views.ReportsListView.as_view(), name="list"),
-    url(
-        r"^standard_report/$",
+    path("list/", views.ReportsListView.as_view(), name="list"),
+    path("standard_report/",
         views.ReportsStandardReportView.as_view(),
         name="standard_report",
     ),
-    url(
-        r"^country_report/$",
-        views.CountryReportView.as_view(),
-        name="country_report",
-    ),
-    url(r"^(?P<query_id>\d+)/$", views.ReportsDetailView.as_view(), name="detail"),
-    url(r"^(?P<query_id>\d+)/json/$", views.ReportDataJsonView.as_view(), name="json"),
-    url(r"^snapshot/$", views.SnapshotView.as_view(), name="snapshot"),
-    url(r"^playground/$", views.PlaygroundView.as_view(), name="playground"),
-    url(
-        r"^(?P<query_id>\d+)/download/$",
+    path("country_report/", views.CountryReportView.as_view(), name="country_report"),
+    path("<query_id>/", views.ReportsDetailView.as_view(), name="detail"),
+    path("<query_id>/json/", views.ReportDataJsonView.as_view(), name="json"),
+    path("snapshot/", views.SnapshotView.as_view(), name="snapshot"),
+    path("playground/", views.PlaygroundView.as_view(), name="playground"),
+    path("<query_id>/download/",
         views.DownloadReportsView.as_view(),
         name="download",
     ),
-    url(r"^schema/$", RedirectView.as_view(pattern_name="explorer_schema")),
-    url(r"^report/pdf/$", views.Pdf.as_view(), name="report_pdf"),
+    path("schema/", RedirectView.as_view(pattern_name="explorer_schema")),
+    path("report/pdf/", views.Pdf.as_view(), name="report_pdf"),
 ]
 
 urlpatterns = [
-    url(r"^$", RedirectView.as_view(url="/about")),
-    url(
-        r"^password/reset/$",
+    path("", RedirectView.as_view(url="/about")),
+    path("password/reset/",
         auth_views.PasswordResetView.as_view(
             template_name="auth/password_reset_form.html"
         ),
         name="password_reset",
     ),
-    url(
-        r"^password_reset/done/$",
+    path("password_reset/done/",
         auth_views.PasswordResetDoneView.as_view(
             template_name="auth/password_reset_done.html"
         ),
         name="password_reset_done",
     ),
-    url(
-        r"^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-"
-        r"[0-9A-Za-z]{1,20})/$",
+    path(
+        "reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(
             template_name="auth/password_reset_confirm.html"
         ),
         name="password_reset_confirm",
     ),
-    url(
-        r"^reset/done/$",
+    path("reset/done/",
         auth_views.PasswordResetCompleteView.as_view(
             template_name="auth/password_reset_complete.html"
         ),
         name="password_reset_complete",
     ),
-    url(r"^product/", include((product_patterns, "insitu"), namespace="product")),
-    url(
-        r"^requirement/",
+    path("product/", include((product_patterns, "insitu"), namespace="product")),
+    path("requirement/",
         include((requirement_patterns, "insitu"), namespace="requirement"),
     ),
-    url(r"^data/", include((data_patterns, "insitu"), namespace="data")),
-    url(r"^provider/", include((provider_patterns, "insitu"), namespace="provider")),
-    url(r"", include((auth_patterns, "insitu"), namespace="auth")),
-    url(r"^reports/", include((reports_patterns, "insitu"), namespace="reports")),
-    url(r"manage$", views.Manager.as_view(), name="manage"),
-    url(r"help$", views.HelpPage.as_view(), name="help"),
-    url(r"about$", views.AboutView.as_view(), name="about"),
-    url(r"^docs/(?P<path>.*)$", serve, {"document_root": settings.DOCS_ROOT}),
-    url(
-        r"^docs/guide.html",
+    path("data/", include((data_patterns, "insitu"), namespace="data")),
+    path("provider/", include((provider_patterns, "insitu"), namespace="provider")),
+    path("", include((auth_patterns, "insitu"), namespace="auth")),
+    path("reports/", include((reports_patterns, "insitu"), namespace="reports")),
+    path("manage", views.Manager.as_view(), name="manage"),
+    path("help", views.HelpPage.as_view(), name="help"),
+    path("about", views.AboutView.as_view(), name="about"),
+    path("docs/<path>", serve, {"document_root": settings.DOCS_ROOT}),
+    path("docs/guide.html",
         serve,
         {"document_root": settings.DOCS_ROOT},
         name="user_manual",
     ),
-    url(r"crashme$", views.Crashme.as_view(), name="crashme"),
-    url(
-        r"^user/records/$",
+    path("crashme", views.Crashme.as_view(), name="crashme"),
+    path("user/records/",
         views.UserRecordsView.as_view(),
         name="user_records",
     ),
-    url(
-        r"user/logs",
+    path("user/logs",
         views.ExportLogs.as_view(),
         name="export_logs",
     ),
-    url(
-        r"user/change_name",
+    path("user/change_name",
         views.ChangeNameEmail.as_view(),
         name="change_name_email",
     ),
