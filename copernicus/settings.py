d5bd9d4f2f3634e5ddd307cd6535381b49bc550c
gabrielastrezea <strezea_gabriela@yahoo.com>
2023-09-13 12:42:18 +0300

    Add Edit, Delete pages and filters on Listing (#155)
    * Add Edit, Delete and filters on listing. And permissions for CRUD operations
    
    * Fix image field on edit

--- copernicus/settings.py
+++ copernicus/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_elasticsearch_dsl",
+    "django_filters",
     "bootstrap3",
     "django_fsm",
     "hijack",
--- use_cases/urls.py
+++ use_cases/urls.py
@@ -5,6 +5,8 @@ from use_cases import views
 urlpatterns = [
     path("", views.UseCaseListView.as_view(), name="list"),
     path("<int:pk>/", views.UseCaseDetailView.as_view(), name="detail"),
+    path("<int:pk>/edit", views.UseCaseEditView.as_view(), name="edit"),
+    path("<int:pk>/delete", views.UseCaseDeleteView.as_view(), name="delete"),
     path("add/", views.UseCaseAddView.as_view(), name="add"),
     path(
         "<int:pk>/transition/<str:source>-to-<str:target>/<slug:transition>/",
