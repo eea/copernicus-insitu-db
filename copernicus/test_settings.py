092b0c541a15b22edd5f2d7562fb074387272c2d
Diana Boiangiu <boiangiudiana58@gmail.com>
2024-09-12 12:41:27 +0300

    Fix tests

--- copernicus/test_settings.py
+++ copernicus/test_settings.py
@@ -7,6 +7,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    },
+    "explorer": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "explorer.sqlite3"),
     }
 }
 
